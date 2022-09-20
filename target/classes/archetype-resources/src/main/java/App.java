0ea9a6b7a0001915305f468f97ee5e8a5fd2f909
BranchOfMadness <skillfull@gmail.com>
2022-09-20 17:43:05 +0300

    commit_v1

---
+++ target/classes/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $ua.lpnu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
